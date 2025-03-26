41688b0f97bee9393e2e3b2bc36c41177c28c8c2
wacpp5 <will@teadog.com>
2025-03-26 07:34:02 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 import decimal
 
 def convert_decimals(obj):
-    if isinstance(obj, list):
-        return [convert_decimals(i) for i in obj]
+    if isinstance(obj, decimal.Decimal):
+        return float(obj)
     elif isinstance(obj, dict):
         return {k: convert_decimals(v) for k, v in obj.items()}
-    elif isinstance(obj, decimal.Decimal):
-        return float(obj)
+    elif isinstance(obj, list):
+        return [convert_decimals(i) for i in obj]
+    elif isinstance(obj, tuple):
+        return tuple(convert_decimals(i) for i in obj)
+    elif isinstance(obj, set):
+        return {convert_decimals(i) for i in obj}
     else:
         return obj
